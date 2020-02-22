2497c5c034360b0f2b41f14db02cc1fc76af0d9a
Julian <juprince@reed.edu>
2020-02-21 16:32:09 -0800

    Part 2 functions defined

---
+++ ordered.cc
@@ -0,0 +1,180 @@
+// 
+// dict.cc
+//
+// Implementation of the operations for an integer linked list data
+// structure.
+//
+// Homework 03 CSCI 221 Spring 2020
+//
+// Author: ??        (`length` and `remove`) 
+// Author: Jim Fix   (the others)
+//
+// The code below gives the implementation code for a linked list data
+// structure. It defines operations on a `dict::dict*` that references
+// a sequence of integers. These are all packaged within the namespace
+// `dict` and are as follows:
+//
+// * build, buildWith - construct a new linked list (on the heap)
+// * insertAtFront, insertAtEnd - add an integer to the linked list
+// * isEmpty, contains, length, toString - check/report the contents
+// * deleteFront, deleteEnd, remove - remove an integer
+// * destroy - give back the linked list's storage (to the heap)
+//
+// Homework 03 Part 1 asks that you complete the code for
+//
+//     int length(dict* list) { ... }
+//
+// which should traverse the linked list starting at `list->first`
+// and count how many nodes are in that linked list. It then asks
+// you to write the code for
+//
+//     int remove(dict* list, int value) { ... }
+//
+// which searches through the list, looking for a node with its `data`
+// field equal to `value`, and excises that node from the linked list.
+// (See the `deleteFront` and `deleteEnd` code to see how those cases
+// are handled.) If more than one node contains that same value, then
+// it only removes the first node with that value.
+//
+
+#include <string>
+#include <iostream>
+#include "ordered.hh"
+
+namespace ordered {
+
+  // Returns a pointer to an empty dictionary
+  // Missing entires have a default associated value of 'v
+  dict* build(int v) {
+    dict* newDict = new dict;
+    newDict->first = nullptr;
+    newDict->defaultValue = v;
+    return newDict;
+  }
+
+  //Returns the value associated with the entry with key 'k':
+  int get(dict* D, std::string k) {
+    //Check if dict is empty
+    if (D->first == nullptr){
+      return -1; //Returns -1 if first value is nullptr
+    }
+    //Search for k with dict transversal
+    entry* current = D->first;
+    while (current != nullptr){
+      //If given k matches the key for that entry, return that entrys value
+      if(current->key == k){
+        return current->value;
+      }
+      current = current->next;
+    }
+    return -1; //Return -1 if k is not found
+  }
+  
+  //Update dictionary so that value 'v' is associated with 'k'
+  void set(dict* D, std::string k, int v){
+    //If dict is not empty
+    if (D->first != nullptr){
+      //Search for k with dict transversal
+      entry* current = D->first;
+
+      while (current != nullptr){
+        //If k is found, update the value of that entry with v
+        if(current->key == k){
+          current->value = v;
+        }
+        current = current->next;
+      }
+
+      //If k is not found, add an entry to the end with key 'k' and value 'v'
+      entry* newEntry = new entry;
+      newEntry->key = k;
+      newEntry->value = v;
+      newEntry->next = nullptr; //New end
+      //Link old end entry to new end entry 
+      current->next = newEntry;
+    }
+    else {    
+      //If dict is empty, make the first entry with key 'k' and value 'v'
+      entry* newEntry = new entry;
+      newEntry->key = k;
+      newEntry->value = v;
+      newEntry->next = nullptr;
+      D->first = newEntry; 
+    }
+  }//End set
+
+  //Give back a python representation of the dictionary 
+  std::string toString(dict* D){
+    //If dict is empty
+    if (D->first == nullptr) {
+      //Return empty list
+      return "{}";
+    } else {
+
+      std::string s = "{";
+      entry* current = D->first;
+      // Traverse up to the end, placing commas after each `entry`.
+      while (current->next != nullptr) {
+	      s += current->key + ":" + std::to_string(current->value) + ",";
+	      current = current->next;
+      } 
+      // Add the last `entry` (which could happen to be the `first`).
+      s += current->key + ":" + std::to_string(current->value) + "}";
+      
+
+      return s;
+    }
+  }
+
+
+
+
+
+
+
+
+
+  // dict::toString(list):
+  // 
+  // Devises a string for the linked `list`, similar to Python's
+  // reperesentation. An empty list string is "[]", a singleton list
+  // is like "[42]", and a longer list is like "[5, 7, 3]".
+  //
+  std::string toString(dict* list) {
+    if (list->first == nullptr) {
+
+      return "[]";
+
+    } else {
+
+      std::string s = "[";
+      node* current = list->first;
+      // Traverse up to the end, placing commas after each `data`.
+      while (current->next != nullptr) {
+	s += std::to_string(current->data) + ", ";
+	current = current->next;
+      } 
+      // Add the last `data` (which could happen to be the `first`).
+      s += std::to_string(current->data) + "]";
+      return s;
+
+    }
+  }
+
+  // dict::destroy(list):
+  // 
+  // Returns the storage of the nodes and the top-level `dict` struct
+  // back to the heap, calling `delete` on the pointer for each.
+  //
+  // This is a "destructor", using C++'s object-oriented terminology.
+  //
+  void destroy(dict* list) {
+    // Delete each node, which performs the `delete` on each.
+    while (!isEmpty(list)) {
+      deleteFront(list);
+    }
+    // `delete` the housing struct.
+    delete list;
+  }
+
+} // end namespace dict

b7db3b76537002bc4e6845e89fa37ce03b77a726
Julian <juprince@reed.edu>
2020-02-21 21:19:03 -0800

    toString function implemented

--- queue.cc
+++ queue.cc
@@ -1,4 +1,4 @@
-// 
+//
 // queue.cc
 //
 // Implementation of the operations for an integer queue, structured as
@@ -6,53 +6,81 @@
 //
 // Homework 03 CSCI 221 Spring 2020
 //
-// Author: ??        
+// Author: ??
 //
 
 #include <string>
 #include <iostream>
 #include "queue.hh"
 
-namespace queue {
+namespace queue
+{
 
-  // queue::build():
-  // 
-  // Constructs a new empty `queue::queue` object returning a pointer
-  // to it from the heap.
-  //
-  queue* build(void) {
-    queue* newQ = new queue;
-    newQ->first = nullptr; 
-    newQ->last  = nullptr; 
-    return newQ;
-  }
+// queue::build():
+//
+// Constructs a new empty `queue::queue` object returning a pointer
+// to it from the heap.
+//
+queue *build(void)
+{
+  queue *newQ = new queue;
+  newQ->first = nullptr;
+  newQ->last = nullptr;
+  return newQ;
+}
 
-  // queue::isEmpty(Q):
-  // 
-  // Returns true/false depending on whether `Q` is empty or not.
-  //
-  bool isEmpty(queue* Q) {
-    return (Q->first == nullptr && Q->last == nullptr);
-  }
+// queue::isEmpty(Q):
+//
+// Returns true/false depending on whether `Q` is empty or not.
+//
+bool isEmpty(queue *Q)
+{
+  return (Q->first == nullptr && Q->last == nullptr);
+}
 
-  // queue::toString(Q):
-  // 
-  // Returns a string representation of the queue.  An empty queue is
-  // "[]", a queue with only a head item is like "[(42)]", and a
-  // longer queue looks like "[(105) 37 83]".
-  //
-  std::string toString(queue* Q) {
-    return "[]"; // Returns a bogus value. Fix this!
+// queue::toString(Q):
+//
+// Returns a string representation of the queue.  An empty queue is
+// "[]", a queue with only a head item is like "[(42)]", and a
+// longer queue looks like "[(105) 37 83]".
+//
+std::string toString(queue *Q){
+  if (Q->first == nullptr && Q->last == nullptr)
+  {
+    //Return empty queue if first and last nodes are nullptr
+    return "[]";
   }
+  else if (Q->first == Q->last)
+  {
+    //Return queue if it only has one element
+    return "[(" + std::to_string(Q->first->data) + ")]";
+  }
+  else
+  {
+    node *current = Q->first;
+    std::string s = "[(" + std::to_string(current->data) + ") ";
+    current = current->next;
+    //Traverse up to the end
+    while (current->next != nullptr) {
+      s += std::to_string(current->data);
+      current = current->next;
+    }
+
+    //Add ending bracket 
+    s += "]";
+    return s;
+  }
+}
 
   // queue::enqueue(Q,item):
   //
   // Adds `item` to the end of the given `Q`.
   //
-  void enqueue(queue* Q, int item) {
+  void enqueue(queue * Q, int item)
+  {
     // UNIMPLEMENTED
     // Write for Homework 03, Part 3.
-    return;  // Does nothing. Fix this!
+    return; // Does nothing. Fix this!
   }
 
   // queue::dequeue(Q):
@@ -60,38 +88,40 @@ namespace queue {
   // Assumes `Q` is not empty. Removes and returns the item
   // at the head (the first node) of the queue.
   //
-  int dequeue(queue* Q) {
+  int dequeue(queue * Q)
+  {
     // UNIMPLEMENTED
     // Write for Homework 03, Part 3.
 
     // NOTE: remember to set `Q->last` to `nullptr` when the queue
     // becomes empty!!
 
-    return 0;  // Does nothing. Returns a bogus value. Fix this!
+    return 0; // Does nothing. Returns a bogus value. Fix this!
   }
 
   // queue::head(Q):
   //
   // Returns the item stored in the first node of the queue.
   //
-  int head(queue* Q) {
+  int head(queue * Q)
+  {
     // UNIMPLEMENTED
     // Write for Homework 03, Part 3.
     return 0; // Returns a bogus value. Fix this!
   }
 
   // queue::destroy(Q):
-  // 
+  //
   // Returns the storage of the nodes and the top-level `queue` struct
   // back to the heap.
   //
   // This is a "destructor", using C++'s object-oriented terminology.
   //
-  void destroy(queue* Q) {
+  void destroy(queue * Q)
+  {
     // UNIMPLEMENTED
     // Write for Homework 03, Part 3.
     return; // Does nothing. Fix this!
-
   }
 
 } // end namespace queue
